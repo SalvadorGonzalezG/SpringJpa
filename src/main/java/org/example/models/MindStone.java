26375e71355c3f20b49f0c1e20a7effe3192e7e7
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-08-09 01:48:28 -0600

    [ADD] -Pattern pototype & instance in main.java

--- src/main/java/org/example/models/MindStone.java
+++ src/main/java/org/example/models/MindStone.java
@@ -1,8 +1,12 @@
 package org.example.models;
 
 import lombok.ToString;
+import lombok.extern.java.Log;
+
+import java.io.*;
 
 @ToString
+@Log
 public class MindStone extends Stone{
 
      private final static String COLOR = "AMARILLO";
@@ -18,5 +22,28 @@ public class MindStone extends Stone{
     public void usePower() {
         // Business Logic
         System.out.print("Mind control in stone" + super.toString());
+     }
+     public MindStone getPrototipe(){
+        try(
+                // Convert object into byte (Serializando)
+                final var bos = new ByteArrayOutputStream();
+                final var oos = new ObjectOutputStream(bos);
+                ){
+            // Serialize object (clone) objeto se auto serializa y se auto clona
+            oos.writeObject(this);
+            oos.flush();
+            try(
+                    // Deserializando
+                    final var bis = new ByteArrayInputStream(bos.toByteArray());
+                    final var ois = new ObjectInputStream(bis);
+                    ){
+                // Cast
+                return (MindStone) ois.readObject();
+            }
+        }catch (IOException | ClassNotFoundException e){
+            log.warning("Cant cast or read class");
+            throw new RuntimeException(e.getMessage());
+        }
+
      }
 }
